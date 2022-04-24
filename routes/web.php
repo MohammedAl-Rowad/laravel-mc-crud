6cac0b83f14d8f2d6cb67e02184db34e1e6ecc49
Rowadz <38977667+MohammedAl-Rowad@users.noreply.github.com>
2022-04-24 21:32:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +16,17 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-use Illuminate\Http\Request;
-
-
 Route::get('/', function () {
     return view('create-post');
 })->name('home');
+
+Route::post('/create-post', function (Request $request) {
+    $id = DB::table('posts')->insertGetId([
+        'body' => $request->input('body'),
+        'title' => $request->input('title'),
+    ]);
+    return redirect()->route('home');
+})->name('create-post');
 
 
 Route::get('/posts', function () {
@@ -29,26 +35,13 @@ Route::get('/posts', function () {
     ]);
 })->name('posts');
 
+
 Route::get('/post/{id}', function ($id) {
     return view('post', [
         'post' => Post::findOrFail($id)
     ]);
-})->name('posts-by-id');
+})->name('post-by-id');
 
-
-Route::get('/delete-post/{id}', function ($id) {
-    Post::destroy($id);
-    return redirect('/posts');
-})->name('delete-post');
-
-
-Route::post('/create-post', function (Request $request) {
-    $id = DB::table('posts')->insertGetId([
-        // 'title' => $request->input('title'),
-        'body' => $request->input('body'),
-    ]);
-    return redirect()->route('posts-by-id', ['id' => $id]);
-})->name('create-post');
 
 Route::get('/update-post/{id}', function ($id) {
     return view('create-post', [
@@ -56,10 +49,26 @@ Route::get('/update-post/{id}', function ($id) {
     ]);
 })->name('update-post-form');
 
+
 Route::post('/update-post/{id}', function (Request $request) {
     $id = $request->route('id');
     $post = Post::findOrFail($id);
     $post->body = $request->input('body');
+    $post->title = $request->input('title');
     $post->save();
-    return redirect()->route('posts-by-id', ['id' => $id]);
+    return redirect()->route('post-by-id', ['id' => $id]);
 })->name('update-post');
+
+
+Route::get('/delete-post/{id}', function ($id) {
+    Post::destroy($id);
+    return redirect()->route('home');
+})->name('delete-post');
+
+
+// delete resource -> Route::delete
+// update resource -> Route::patch
+// create resource -> Route::post
+// get resource -> Route::get
+
+// change the resource completly resource -> Route::put
