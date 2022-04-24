3b7d15f59c03637c8ea00c30e473b0fd3c94537e
Rowadz <38977667+MohammedAl-Rowad@users.noreply.github.com>
2022-04-24 21:33:50 +0200

    Update posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,28 @@
 @extends('main')
 
+
 @section('content')
+
+
 <div class="container mt-4">
-        
     <div class="row">
-       @foreach ($posts as $post)
-           <div class="col-4">
-               <div class="card">
-                   <div class="card-body">
-                       <h5 class="card-title"></h5>
-                       <p class="card-text text-truncate">
-                           {{ $post->body }}
-                       </p>
-                       <a href="{{ route('posts-by-id', ['id' => $post->id]) }}" class="btn btn-primary">See deatils</a>
-                   </div>
-               </div>
-           </div>
-       @endforeach
-   </div>
-   {{ $posts->links() }}
+        @foreach ($posts as $post)
+                <div class="col-4">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            {{ $post->title; }}
+                        </h5>
+                        <p class="card-text text-truncate">
+                            {{ $post->body; }}
+                        </p>
+                        <a href="{{ route('post-by-id', ['id' => $post->id]) }}" class="btn btn-primary">
+                            See details
+                        </a>
+                    </div>
+                </div>
+        @endforeach
+    </div>
+    {{ $posts->links('pagination::bootstrap-5') }}
 </div>
+
 @endsection
